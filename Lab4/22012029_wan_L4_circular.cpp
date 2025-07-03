99f2384fe2f7e04ea572b0cf779afe2c6aabd6d4
Wan's Inventory <ayunihumairq@gmail.com>
2025-07-03 17:05:11 +0800

    Add files via upload

---
+++ Lab4/22012029_wan_L4_circular.cpp
@@ -0,0 +1,66 @@
+/*
+ID = 22012029
+Name = Wan Ayuni Humairaq binti Wan Nami
+*/
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    string name;
+    Node* next;
+    Node(string name) : name(name) , next(NULL) {}
+
+    
+
+};
+
+class CircularLinkedList {
+public:
+
+string name;
+Node* head = NULL;
+Node* tails = NULL;
+
+    void addNode(string name) {
+        Node* newNode = new Node(name);
+        if (head == NULL) {
+            head = newNode;
+            tails = newNode;
+            newNode->next = head; 
+        } else {
+            tails->next = newNode;
+            tails = newNode;
+            tails->next = head; 
+        }
+    }
+
+    void display() {
+        Node* current = head;
+
+       do {
+        cout << current->name << "-> ";
+        current = current->next;
+    } while (current != head);
+cout << current->name << endl;
+
+}
+};
+
+int main() {
+
+
+CircularLinkedList cll;
+
+cll.addNode("Ali");
+cll.addNode("Ahmed"); 
+cll.addNode("Alice");
+cll.addNode("Hamza");
+
+cll.display();
+
+
+    return 0;
+
+}
